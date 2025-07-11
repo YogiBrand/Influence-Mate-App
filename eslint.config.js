b807a081d176e4c40ef883cbe318ab95a20d0c72
YogiBrand <146953986+YogiBrand@users.noreply.github.com>
2025-07-11 12:01:48 -0400

    Fix ESLint warnings and ignore build dir

--- eslint.config.js
+++ eslint.config.js
@@ -13,23 +13,21 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'prefer-const': 'off',
-      'no-case-declarations': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      'no-useless-escape': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-    },
+      plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'prefer-const': 'off',
+        'no-case-declarations': 'off',
+        '@typescript-eslint/no-empty-object-type': 'off',
+        'no-useless-escape': 'off',
+        '@typescript-eslint/no-require-imports': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+      },
   }
 );
